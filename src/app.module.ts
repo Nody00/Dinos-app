b04293d21759e33673af2ed6edb1cf74c37e56d5
nody <dinokrcicprof@gmail.com>
2026-01-11 22:02:21 +0100

    added throttling

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { EmailModule } from './email/email.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventsCommonModule } from './common/events/events-common.module';
 import { UsersModule } from './users/users.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -13,12 +14,31 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
       envFilePath: `.env`,
     }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'short',
+          ttl: 1000,
+          limit: 3,
+        },
+        {
+          name: 'medium',
+          ttl: 10000,
+          limit: 20,
+        },
+        {
+          name: 'long',
+          ttl: 60000,
+          limit: 100,
+        },
+      ],
+    }),
     PrismaModule,
     EventsCommonModule,
     EmailModule,
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ThrottlerGuard],
 })
 export class AppModule {}
